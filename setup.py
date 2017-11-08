e17151935838b523793716374ffa40ad5f31ab57
Christopher Helm <training.helm@gmail.com>
2017-11-08 19:49:22 +0100

    fix(httpsig): hotfix remove from library

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ if sys.version_info < (2, 7, 9):
 install_requires = [
     'pyjwt==1.3.0',
     requests,
-    'six>=1.8.0',
-    'httpsig==1.1.2'
+    'six>=1.8.0'#,
+    #'httpsig==1.1.2'
 ]
 
 class PyTest(TestCommand):
